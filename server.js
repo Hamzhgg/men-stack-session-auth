756850c3ef3232b83571cc1936af44a94afd9b65
Hamzhgg <gharibehhamzh@gmail.com>
2024-10-20 15:04:44 +0300

    /men-stack-session-auth

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+require('dotenv').config();
+require('./config/database');
+
+// Controllers
+const authController = require('./controllers/auth');
+
+const app = express();
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+// MIDDLEWARE
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride('_method'));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+
+// ROUTES
+app.get('/', async (req, res) => {
+  res.render('index.ejs');
+});
+
+app.use('/auth', authController);
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`The express app is ready on port ${port}!`);
+});
